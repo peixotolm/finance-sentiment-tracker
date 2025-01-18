abd5c640b300ff40d2d3a8dd7f1360e2d4e17171
peixotolm <luis.peixoto@fanduel.com>
2025-01-18 18:18:57 +0000

    sentiments and stock endpoints fully working

--- app/core/database.py
+++ app/core/database.py
@@ -2,6 +2,9 @@ from decouple import config
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from app.core.base import Base
+from app.models.product import Product  # noqa: F401
+from app.models.sentiment import SentimentData  # noqa: F401
+from app.models.stock import StockData  # noqa: F401
 
 DATABASE_URL = config("DATABASE_URL", default="sqlite:///test.db")
 
---
+++ app/models/__init__.py
@@ -0,0 +1,3 @@
+from app.models.product import Product  # noqa: F401
+from app.models.sentiment import SentimentData  # noqa: F401
+from app.models.stock import StockData  # noqa: F401
